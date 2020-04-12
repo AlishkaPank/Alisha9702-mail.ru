e840961c70cb1eff03d4377b7607c72f8405ee00
AlishkaPank <62702196+AlishkaPank@users.noreply.github.com>
2020-04-12 04:22:01 +0300

    Update Move.java

--- labs/Шашки/Move.java
+++ labs/Шашки/Move.java
@@ -5,8 +5,6 @@ import java.util.*;
 public class Move {
     private final HashMap<String, Command> commands = new HashMap<>();
     private final List<Command> undoCommands = new LinkedList<>();
-    private final List<Command> redoCommands = new LinkedList<>();
-    private int numberOfDeletedCommands = 0;
 
     public void register(String commandName, Command command){
         commands.put(commandName, command);
@@ -16,21 +14,11 @@ public class Move {
         Command command = commands.get(commandName);
         command.execute();
         undoCommands.add(command);
-        redoCommands.add(command);
     }
 
     public void undo(){
         if (undoCommands.size() > 0){
             undoCommands.remove(undoCommands.size() - 1).undo();
-            ++numberOfDeletedCommands;
-        }
-    }
-
-    public void redo(){
-        if (numberOfDeletedCommands > 0){
-            redoCommands.get(redoCommands.size() - 1 - numberOfDeletedCommands).execute();
-        } else if (redoCommands.size() > 0){
-            redoCommands.get(redoCommands.size() - 1).execute();
         }
     }
 }

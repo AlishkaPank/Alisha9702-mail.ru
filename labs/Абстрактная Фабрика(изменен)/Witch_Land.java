acd46682a89e558b879797077aa8642ece49808e
AlishkaPank <62702196+AlishkaPank@users.noreply.github.com>
2020-04-16 04:34:33 +0300

    Create Witch_Land.java

---
+++ labs/Абстрактная Фабрика(изменен)/Witch_Land.java
@@ -0,0 +1,12 @@
+package abstractFactory.concreteProducts;
+
+import abstractFactory.Land;
+
+public class Witch_Land implements Land {
+
+    public void earthquake() {
+
+        System.out.println("Ведьма земли вызывает землетрясение.");
+    }
+
+}

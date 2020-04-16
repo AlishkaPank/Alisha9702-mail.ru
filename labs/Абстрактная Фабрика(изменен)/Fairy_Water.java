c701447cc6f7a7f16a04d233309eb4425ecf19ea
AlishkaPank <62702196+AlishkaPank@users.noreply.github.com>
2020-04-16 04:32:55 +0300

    Create Fairy_Water.java

---
+++ labs/Абстрактная Фабрика(изменен)/Fairy_Water.java
@@ -0,0 +1,12 @@
+package abstractFactory.concreteProducts;
+
+import abstractFactory.Water;
+
+public class Fairy_Water implements Water {
+
+    public void tsunami() {
+
+        System.out.println("Фея воды вызывает цунами.");
+    }
+
+}

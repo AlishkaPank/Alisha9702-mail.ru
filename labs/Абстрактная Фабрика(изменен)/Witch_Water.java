388d7da995dfa41a37cfb24b1d529b55af91c79f
AlishkaPank <62702196+AlishkaPank@users.noreply.github.com>
2020-04-16 04:35:00 +0300

    Create Witch_Water.java

---
+++ labs/Абстрактная Фабрика(изменен)/Witch_Water.java
@@ -0,0 +1,11 @@
+package abstractFactory.concreteProducts;
+
+import abstractFactory.Water;
+
+public class Witch_Water implements Water {
+    public void tsunami() {
+
+        System.out.println("Ведьма воды вызывает цунами.");
+    }
+
+}

ad3569047ad22c2864934954334bf2e9cb1d5653
AlishkaPank <62702196+AlishkaPank@users.noreply.github.com>
2020-04-16 04:31:35 +0300

    Create Fairy_Air.java

---
+++ labs/Абстрактная Фабрика(изменен)/Fairy_Air.java
@@ -0,0 +1,13 @@
+package abstractFactory.concreteProducts;
+
+import abstractFactory.Air;
+
+public class Fairy_Air implements Air {
+
+    public void causes_tornado(){
+
+        System.out.println("Фея воздуха запускает торнадо.");
+
+    }
+
+}

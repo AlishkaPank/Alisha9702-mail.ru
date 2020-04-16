63fc654499159dca9c3ffe98cb991b8295031f3b
AlishkaPank <62702196+AlishkaPank@users.noreply.github.com>
2020-04-16 04:33:47 +0300

    Create Witch_Air.java

---
+++ labs/Абстрактная Фабрика(изменен)/Witch_Air.java
@@ -0,0 +1,11 @@
+package abstractFactory.concreteProducts;
+
+import abstractFactory.Air;
+
+public class Witch_Air implements Air {
+
+    public void causes_tornado() {
+
+        System.out.println("Ведьма воздуха запускает торнадо.");
+    }
+}

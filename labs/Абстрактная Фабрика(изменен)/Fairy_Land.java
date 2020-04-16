7187ce3aa71b87c407b0b9cf7eb91c40b6f54f3b
AlishkaPank <62702196+AlishkaPank@users.noreply.github.com>
2020-04-16 04:32:29 +0300

    Create Fairy_Land.java

---
+++ labs/Абстрактная Фабрика(изменен)/Fairy_Land.java
@@ -0,0 +1,11 @@
+package abstractFactory.concreteProducts;
+
+import abstractFactory.Land;
+
+public class Fairy_Land implements Land {
+
+    public void earthquake() {
+
+        System.out.println("Фея земли вызывает землетрясение.");
+    }
+}
